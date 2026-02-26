ccf738dd5df72c5bca5772271e7c7aaa9dcdc156
sosyologramofficial <sosyologramofficial@gmail.com>
2026-02-26 19:24:45 +0300

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+import database as db
+
+workers = 2
+timeout = 120
+bind = "0.0.0.0:10000"
+
+def post_fork(server, worker):
+    from api import resume_incomplete_tasks
+    db.init_db()
+    resume_incomplete_tasks()
