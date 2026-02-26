9acfb42a56c83c633f185b11a35c934328a41ed5
sosyologramofficial <sosyologramofficial@gmail.com>
2026-02-26 22:46:56 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,5 +6,8 @@ bind = "0.0.0.0:10000"
 
 def post_fork(server, worker):
     from api import resume_incomplete_tasks
+    print("[POST_FORK] Starting db.init_db...")
     db.init_db()
+    print("[POST_FORK] db.init_db done, starting resume...")
     resume_incomplete_tasks()
+    print("[POST_FORK] Done, worker ready.")
